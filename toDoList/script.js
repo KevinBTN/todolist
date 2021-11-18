334826156fc0350954bc3935d2b497c77b378b70
Kevin BRANTONE <kbrantone@gmail.com>
2021-11-18 17:07:24 +0100

    MaJ interface
    - modification de l'interface en concordance avec les cahier des charges
    - ajout de la fonction qui enregistre l'objet tache dans le localStorage quand on click sur ajouter une tache
    - ajout des checkbox pour valider les tâches terminé
    - ajout de la zone de text pour détailler la tâche ajouté
    - pour l'instant quand on créé un nouvel objet tâche le nouvel objet prend bien la date du date picker

--- toDoList/script.js
+++ toDoList/script.js
@@ -20,5 +20,19 @@
 
 /*Kevin*/
 /*Variables*/
+const date = $("#datepicker").get(0);  //selecteur pour le date picker
+const ajouterTache = $("#ajouterTache").get(0); //selecteur pour le bouton d'ajou de tâche
 /*Fonctions*/
+$( function() {   //fonction pour rafraichir et activer le datepicker 
+    $( "#datepicker" ).datepicker();
+  } );
+
 /*Executions*/
+$("#ajouterTache").click(function(){  //click sur bouton ajouter à la liste
+    const nouvelleTache = new Task(12, "test", date.value, "haute");   //créer une nouvelle instance de l'objet Task 
+    localStorage.setItem(nouvelleTache.nom, JSON.stringify(nouvelleTache)); //ajoute la tache à la mémoire local pour garder les informations utilise JSON.stringify pour socker lobjet en tant que string
+})
+const lireObjet = function(key){    //fonction pour récupérer la tache enregistré comme string sous forme d'objet
+    var value = this.localStorage.getItem(key);
+    return value && JSON.parse(value);
+}
